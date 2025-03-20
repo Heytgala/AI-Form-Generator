5c035b1392a2fd9d4089217113d9ae5dc46b4ff5
Heytgala <heytgala26@gmail.com>
2025-03-20 17:40:11 -0500

    Header Page (Home)

---
+++ my-app/app/(home)/layout.tsx
@@ -0,0 +1,23 @@
+import Logo from '@/components/Logo'
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+        <div className='border-b'>
+            <nav className='flex items-center justify-between max-w-7xl mx-auto py-2'>
+                <Logo/>
+                <div>
+                    <Button variant={"link"}>Dashboard</Button>
+                    <UserButton/>
+                </div>
+            </nav>
+        </div>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
---
+++ my-app/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>
+        <h1 className='font-extrabold text-2xl'>AI Form Generator</h1>
+    </div>
+  )
+}
+
+export default Logo
