17658c59e0edeb6c6150c30596f104c720e75d7c
Heytgala <heytgala26@gmail.com>
2025-03-20 18:17:55 -0500

    Bug Fix

--- my-app/app/(home)/page.tsx
+++ my-app/app/(home)/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
-      
+      <HeroSection/>
     </div>
   )
 }
---
+++ my-app/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className='text-center'>
+            <h1 className='text-4xl font-bold'>Build AI Driven Form 🚀</h1>
+            <p>Leverage the "Power of AI" to create responsive and dynamic "Forms"</p>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
